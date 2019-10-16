088781791db27266e116c5521f838ef21defb000
MendesRuanito <ruanmendesmarques@gmail.com>
2019-10-16 02:09:38 -0300

    Menu & Hover

--- index.php
+++ index.php
@@ -5,10 +5,39 @@ require_once("logica-usuario.php");
 <h1>Bem Vindo!</h1>
 
 <?php
-		if (usuarioEstaLogado()) {
-				?> <p class="alert alert-info">Voce está logado como <?=usuarioLogado()?>. <a href="logout.php">Deslogar</a></p><?php
-		} else {
-				if (isset($_GET['login']) && $_GET['login']==0) { ?>
+        if (usuarioEstaLogado()) {
+            ?>
+						<br>
+
+						 <div class="row">
+							 <a href="aluno-formulario.php" class="col-md-auto">
+						<div class="menu" style="text-align: center">
+						<img src="img/Index/computer.svg" class="rounded mx-auto  d-block" width="128px" alt="...">
+						<br>
+						<font size="6px">Cadastro</font>
+					</div>
+				</a>
+				 <a href="aluno-lista.php" class="col-md-auto">
+					<div class="menu" style="text-align: center">
+						<img src="img/Index/research-1.svg" class="rounded mx-auto d-block" width="128px" alt="...">
+						<br>
+						<font size="6px">Lista</font>
+					</div>
+				</a>
+				<a href="aluno-busca.php" class="col-md-auto">
+					<div class="menu" style="text-align: center">
+						<img src="img/Index/research.svg" class="rounded mx-auto d-block" width="128px" alt="...">
+						<br>
+						<font size="6px">Pesquisa</font>
+					</div>
+					<a/>
+					</div>
+					<br>
+					<br>
+
+						<?php
+        } else {
+            if (isset($_GET['login']) && $_GET['login']==0) { ?>
 					<div class="alert alert-danger alert-dismissible fade show" role="alert">
 						Email e/ou Senha incorretos!
 						<button type="button" class="close" data-dismiss="alert" aria-label="Close">
@@ -16,7 +45,7 @@ require_once("logica-usuario.php");
 						</button>
 					</div>
 <?php }
-				if (isset($_GET['login']) && $_GET['login']==1) { ?>
+            if (isset($_GET['login']) && $_GET['login']==1) { ?>
 			<div class="alert alert-danger alert-dismissible fade show" role="alert">
 				Esta operação necessita de um login!
 				<button type="button" class="close" data-dismiss="alert" aria-label="Close">
@@ -40,13 +69,13 @@ require_once("logica-usuario.php");
 		</tr>
 	</table>
 </form>
+
 <?php
-		}
+        }
 require_once("rodape.php"); ?>
 <script>
 		document.getElementById('inicio').classList.add('active'); // Adiciona classe
 		document.getElementById('cad').classList.remove('active'); // Adiciona classe
 		document.getElementById('pes').classList.remove('active'); // Adiciona classe
 		document.getElementById('lis').classList.remove('active'); // Adiciona classe
-
 </script>
