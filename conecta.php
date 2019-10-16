e7666902e1a4b5166b23b19ee996ca124a3ea62a
MendesRuanito <ruanmendesmarques@gmail.com>
2019-10-16 02:10:00 -0300

    DB

--- conecta.php
+++ conecta.php
@@ -1,3 +1,3 @@
 <?php
-//$connection = mysqli_connect("mysql380.umbler.com", "mendesruanito", "ruan2014", "escolaruanito");
-$connection = mysqli_connect("localhost", "root", "", "escola");
+$connection = mysqli_connect("mysql380.umbler.com", "mendesruanito", "ruan2014", "escolaruanito");
+//$connection = mysqli_connect("localhost", "root", "", "escola");
